c53f1e792c34c8b0297942e9671474e68acf43dd
luc.evans@mymail.champlain.edu <luc.evans@mymail.champlain.edu>
2025-01-22 13:47:58 -0500

    Project Management .ps1

---
+++ week2/champlainchromecheck.ps1
@@ -0,0 +1,9 @@
+﻿$find_chrome = get-process | where-object { ($_.Name -like "*chrome*") }
+$web = "https://champlain.edu"
+
+if ($find_chrome) {
+    $chrome_id = (get-process -Name "chrome").id
+    Stop-Process -Id $chrome_id}
+else {
+    Start-Process -FilePath "chrome.exe" -Argument $web
+}
