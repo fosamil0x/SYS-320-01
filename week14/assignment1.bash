47b3dcd3de86f4c99e3fbb9d404a5ef529dcd7eb
fosamil0x <luc.evans@mymail.champlain.edu>
2025-04-16 14:35:03 -0400

    Champlain Courses and Bash assignment

--- week14/assignment1.bash
+++ week14/assignment1.bash
@@ -37,6 +37,20 @@ echo ""
 # Example input: JOYC 310
 # Example output: See the screenshots in canvas
 
+function checkClassroom (){
+
+echo -n "Provide a classroom to check: "
+read classroom
+
+echo ""
+echo "Classes in $classroom:"
+cat "$courseFile" | cut -d';' -f1,2,5,7 | \
+grep -v "/" | grep -v "\.\.\." | \
+sed 's/;/ | /g'
+echo ""
+
+}
+
 # TODO - 2
 # Make a function that displays all the courses that has availability
 # (seat number will be more than 0) for the given course code
@@ -44,12 +58,31 @@ echo ""
 # Example input: SEC
 # Example output: See the screenshots in canvas
 
+function displaySeatsInCategory(){
+
+echo -n "Provide a category to check seats for: "
+read category
+
+echo ""
+echo "Open seats in $category classes"
+newlines="$(cat $courseFile | grep $category)"
+echo "$newlines" | while read -r line;
+do
+	num=$(echo "$line" | cut -d';' -f4)
+	if [[ $num -gt 0 ]]
+		then
+			echo "$line" | cut -d';' -f 1,2,3,4,5,6,7,8,9,10 | sed 's/;/ | /g'
+	fi
+done
+}
+
 while :
 do
 	echo ""
 	echo "Please select and option:"
 	echo "[1] Display courses of an instructor"
 	echo "[2] Display course count of instructors"
+	echo "[3] Display courses in a given classroom"
 	echo "[5] Exit"
 
 	read userInput
@@ -65,6 +98,10 @@ do
 	elif [[ "$userInput" == "2" ]]; then
 		courseCountofInsts
 
+	elif [[ "$userInput" == "3" ]]; then
+		checkClassroom
+	elif [[ "$userInput" == "4" ]]; then
+		displaySeatsInCategory
 	# TODO - 3 Display a message, if an invalid input is given
 	fi
 done
